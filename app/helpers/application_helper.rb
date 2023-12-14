2edd7ec4e9ea31efbfbf737df3bb63fddfafa521
Cezary Kłos <cezary.klos@gmail.com>
2023-12-14 15:58:17 +0100

    Print servers hostname

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,8 @@ module ApplicationHelper
   def nested_dom_id(*args)
     args.map { |arg| arg.respond_to?(:to_key) ? dom_id(arg) : arg }.join("_")
   end
+
+  def server_hostname
+    @hostname ||= `hostname`.strip
+  end
 end
